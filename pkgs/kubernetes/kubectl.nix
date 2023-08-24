61dd1b9ca0c93a702414c1d355eba2544d74da0e
Clément Dubreuil <clement@dubreuil.dev>
2023-08-24 16:06:19 +0200

    Update kubectl

--- pkgs/kubernetes/kubectl.nix
+++ pkgs/kubernetes/kubectl.nix
@@ -1,15 +1,15 @@
 { callPackage }:
 callPackage ../../lib/genGoBinary.nix rec {
   name = "kubectl";
-  version = "1.28.0";
+  version = "1.28.1";
   arch = {
     x86_64-linux = {
       url = "https://dl.k8s.io/release/v${version}/bin/linux/amd64/kubectl";
-      sha256 = "4717660fd1466ec72d59000bb1d9f5cdc91fac31d491043ca62b34398e0799ce";
+      sha256 = "e7a7d6f9d06fab38b4128785aa80f65c54f6675a0d2abef655259ddd852274e1";
     };
     aarch64-linux = {
       url = "https://dl.k8s.io/release/v${version}/bin/linux/arm64/kubectl";
-      sha256 = "f5484bd9cac66b183c653abed30226b561f537d15346c605cc81d98095f1717c";
+      sha256 = "46954a604b784a8b0dc16754cfc3fa26aabca9fd4ffd109cd028bfba99d492f6";
     };
   };
 }

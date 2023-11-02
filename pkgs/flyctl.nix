aaff7a7685b551182999708636b8917f96493443
Clément Dubreuil <clement@dubreuil.dev>
2023-11-02 17:38:36 +0100

    Update packages

--- pkgs/flyctl.nix
+++ pkgs/flyctl.nix
@@ -1,16 +1,16 @@
 { callPackage }:
 callPackage ../lib/genGoBinary.nix rec {
   name = "flyctl";
-  version = "0.1.112";
+  version = "0.1.115";
   aliases = [ "fly" ];
   arch = {
     x86_64-linux = {
       url = "https://github.com/superfly/flyctl/releases/download/v${version}/flyctl_${version}_Linux_x86_64.tar.gz";
-      sha256 = "a94d7859682dc8a824cc2ccae6663bb734f996395ef5432f413320faee6e67a7";
+      sha256 = "4da4b1edca42f91cc220cb69cad922c6f927a377668cb706e99c9a75f3a53f15";
     };
     aarch64-linux = {
       url = "https://github.com/superfly/flyctl/releases/download/v${version}/flyctl_${version}_Linux_arm64.tar.gz";
-      sha256 = "d6fc41629ba6b9ebc92e0c86976c03d86829b6dd8221c582f61da90403d9b8c0";
+      sha256 = "704668c8be5cfd85c4c42353adc2b8eab57444812b439a81cca2b3b8c39f3867";
     };
   };
 }
--- pkgs/kubernetes/cilium.nix
+++ pkgs/kubernetes/cilium.nix
@@ -2,16 +2,16 @@
 callPackage ../../lib/genGoBinary.nix rec {
   pname = "cilium-cli-bin";
   name = "cilium";
-  version = "0.15.11";
+  version = "0.15.12";
   commonCompletion = true;
   arch = {
     x86_64-linux = {
       url = "https://github.com/cilium/cilium-cli/releases/download/v${version}/cilium-linux-amd64.tar.gz";
-      sha256 = "5580835cd4f1e4e0aeaaef26de9887ccdb00803b7ef57f57969a0ceda1656c6c";
+      sha256 = "b2153f02acacfea673ec00f6d092ea0532d85b2601dfeb81938694ec69a2644e";
     };
     aarch64-linux = {
       url = "https://github.com/cilium/cilium-cli/releases/download/v${version}/cilium-linux-arm64.tar.gz";
-      sha256 = "26b4dc17217f6caa1aad50a5e98c69e2256baca6925f8286cad6eeb09a5370d8";
+      sha256 = "e71fbd8af45d6ea35b5f10febe89577038b8c10b3ef24496f280af5a35ccb9c0";
     };
   };
 }

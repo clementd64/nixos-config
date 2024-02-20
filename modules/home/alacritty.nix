5ccec73ab381a38982eb363bac2bb58ac48b4ba4
Clément Dubreuil <clement@dubreuil.dev>
2024-02-20 09:37:46 +0100

    fix: alacritty deprecated config

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -22,9 +22,9 @@ in with lib; {
           size = cfg.size;
         };
 
-        draw_bold_text_with_bright_colors = true;
-
         colors = {
+          draw_bold_text_with_bright_colors = true;
+
           # Default colors
           primary = {
             background = "0x0d1117";

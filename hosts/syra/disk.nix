1636371fd844b4da86cea433bfc3e7e431df89f1
Clément Dubreuil <clement@dubreuil.dev>
2024-04-02 18:35:56 +0200

    Add syra disk

---
+++ hosts/syra/disk.nix
@@ -0,0 +1,44 @@
+{
+  disko.devices = {
+    disk.main = {
+      device = "/dev/disk/by-id/nvme-WDS500G1X0E-00AFY0_21416C800280";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          main = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "/docker" = {
+                  mountpoint = "/var/lib/docker";
+                  mountOptions = [ "compress=zstd" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

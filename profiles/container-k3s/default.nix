e88e34c8511f58c133173fbbb8eb8559e7f8eb8c
Clément Dubreuil <clement@dubreuil.dev>
2024-05-20 19:42:24 +0200

    fix: use vip as server

--- profiles/container-k3s/default.nix
+++ profiles/container-k3s/default.nix
@@ -13,7 +13,7 @@
   clement.k3s = {
     enable = true;
     config = {
-      server = "https://10.0.0.1:6443";
+      server = "https://10.0.0.200:6443";
       token-file = "/run/secrets/k3s-server-token";
       tls-san = [ "10.0.0.200" ];
     };

be39fcfd110873ac38ca6d47316bd3cc80b38f7a
Clément Dubreuil <clement@dubreuil.dev>
2024-06-02 13:27:16 +0200

    update to nixos 24.05

--- flake.nix
+++ flake.nix
@@ -8,9 +8,9 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
-    nixpkgs-stable.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs-stable.url = github:nixos/nixpkgs/nixos-24.05;
     home-manager-stable = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 

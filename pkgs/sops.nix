ba29c979b780ec30f7c87dbceaf62bd38a259427
Clément Dubreuil <clement@dubreuil.dev>
2024-06-28 14:19:01 +0200

    update sops

--- pkgs/sops.nix
+++ pkgs/sops.nix
@@ -1,15 +1,15 @@
 { callPackage }:
 callPackage ../lib/genGoBinary.nix rec {
   name = "sops";
-  version = "3.8.1";
+  version = "3.9.0";
   arch = {
     x86_64-linux = {
       url = "https://github.com/getsops/sops/releases/download/v${version}/sops-v${version}.linux.amd64";
-      sha256 = "d6bf07fb61972127c9e0d622523124c2d81caf9f7971fb123228961021811697";
+      sha256 = "0d65660fbe785647ff4f1764d7f69edf598f79d6d79ebbef4a501909b6ff6b82";
     };
     aarch64-linux = {
       url = "https://github.com/getsops/sops/releases/download/v${version}/sops-v${version}.linux.arm64";
-      sha256 = "15b8e90ca80dc23125cd2925731035fdef20c749ba259df477d1dd103a06d621";
+      sha256 = "596f26de6d4f7d1cc44f9e27bfea3192ef77f810f31f3f4132a417860ab91ebc";
     };
   };
 }

6dab4f65c364ede0937d75c5ae076947ecb75ef2
Clément Dubreuil <clement@dubreuil.dev>
2024-07-30 11:50:48 +0200

    Bump version

--- pkgs/opentofu.nix
+++ pkgs/opentofu.nix
@@ -2,15 +2,15 @@
 callPackage ../lib/genGoBinary.nix rec {
   pname = "opentofu-bin";
   name = "tofu";
-  version = "1.7.2";
+  version = "1.8.0";
   arch = {
     x86_64-linux = {
       url = "https://github.com/opentofu/opentofu/releases/download/v${version}/tofu_${version}_linux_amd64.tar.gz";
-      sha256 = "c601cffe874db3634c4413602e3fa08aded2b70110ea9073733d1bad81377e7b";
+      sha256 = "cb54a998eae5dc5890a8d1adacf9b6fe396a57fc6257a9154ccdebb3035b63b8";
     };
     aarch64-linux = {
       url = "https://github.com/opentofu/opentofu/releases/download/v${version}/tofu_${version}_linux_arm64.tar.gz";
-      sha256 = "ee7ab4ed7eda026d8ac451a2202ede90e387cb634d0fca61d7f5b2a406e8303b";
+      sha256 = "0efb30592f8df710440dccb4447c7e1c8e9a66135dc2d93d8f13e2039c95ae26";
     };
   };
 }
--- pkgs/restic.nix
+++ pkgs/restic.nix
@@ -1,15 +1,15 @@
 { callPackage }:
 callPackage ../lib/genGoBinary.nix rec {
   name = "restic";
-  version = "0.16.5";
+  version = "0.17.0";
   arch = {
     x86_64-linux = {
       url = "https://github.com/restic/restic/releases/download/v${version}/restic_${version}_linux_amd64.bz2";
-      sha256 = "f1a9c39d396d1217c05584284352f4a3bef008be5d06ce1b81a6cf88f6f3a7b1";
+      sha256 = "fec7ade9f12c30bd6323568dbb0f81a3f98a3c86acc8161590235c0f18194022";
     };
     aarch64-linux = {
       url = "https://github.com/restic/restic/releases/download/v${version}/restic_${version}_linux_arm64.bz2";
-      sha256 = "41cc6ad3ac5e99ee088011f628fafcb4fa1e4d3846be2333e5c2a3f6143cd0c1";
+      sha256 = "f9ad4d91c181da2968ccdecb5238bf872f824fe1e40253f3347c4025192f19c9";
     };
   };
 }

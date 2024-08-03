213971f5ab05525b540d7816daab71eb75703deb
Clément Dubreuil <clement@dubreuil.dev>
2024-08-04 00:05:21 +0200

    k3s ipv6

--- hosts/engardo/system.nix
+++ hosts/engardo/system.nix
@@ -1,9 +1,7 @@
 { lib, pkgs, ... }:
 let
   cilium-values = {
-    ipam = {
-      mode = "kubernetes";
-    };
+    ipam.mode = "kubernetes";
 
     # kube proxy replacement
     kubeProxyReplacement = true;
@@ -12,21 +10,15 @@ let
     routingMode = "native";
     ipv4NativeRoutingCIDR = "10.42.0.0/16";
 
-    ipv4 = {
-      enabled = true;
-    };
+    ipv4.enabled = true;
     enableIPv4Masquerade = true;
-    bpf = {
-      masquerade = true;
-    };
+    ipv6.enabled = true;
+    enableIPv6Masquerade = false;
+    bpf.masquerade = true;
 
-    operator = {
-      replicas = 1;
-    };
+    operator.replicas = 1;
 
-    hubble = {
-      enabled = false;
-    };
+    hubble.enabled = false;
   };
 
   cilium-manifests = pkgs.callPackage ../../lib/buildCiliumChart.nix {
@@ -86,13 +78,19 @@ in {
       disable-network-policy = true;
       flannel-backend = "none";
       secrets-encryption = true;
+
+      cluster-cidr = "10.42.0.0/16,2a01:4f8:c17:aad::1:0/116";
+      service-cidr = "10.43.0.0/16,fd62:ec16:d507:1af6::/112";
+      kube-controller-manager-arg = "node-cidr-mask-size-ipv6=116";
     };
     manifests.cilium = cilium-manifests;
   };
 
+  boot.kernelModules = [ "ip6_tables" "ip6table_mangle" "ip6table_raw" "ip6table_filter" ];
   networking.firewall.checkReversePath = false;
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -s 10.42.0.0/16 -p tcp -m multiport --dports 6443,10250 -j ACCEPT
+    ip6tables -A nixos-fw -s 2a01:4f8:c17:aad::1:0/116 -p tcp -m multiport --dports 6443,10250 -j ACCEPT
   '';
 
   environment.systemPackages = [

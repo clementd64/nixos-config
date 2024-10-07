8fe4499f0ff9c722323686c5b18eba75e8bf78cd
Clément Dubreuil <clement@dubreuil.dev>
2024-10-07 11:04:08 +0200

    Upgrade

--- pkgs/opentofu.nix
+++ pkgs/opentofu.nix
@@ -2,15 +2,15 @@
 fetchStaticBinary rec {
   pname = "opentofu-bin";
   name = "tofu";
-  version = "1.8.2";
+  version = "1.8.3";
   arch = {
     x86_64-linux = {
       url = "https://github.com/opentofu/opentofu/releases/download/v${version}/tofu_${version}_linux_amd64.tar.gz";
-      sha256 = "9f43888571cbe9cdfe03e81c6cbb93b4aacc014db19373a4e7608aaa7df111d1";
+      sha256 = "77f3cecf737d1b4f09087c1417836e8fdc32080b59daa37bbf1bf736b80cd3b4";
     };
     aarch64-linux = {
       url = "https://github.com/opentofu/opentofu/releases/download/v${version}/tofu_${version}_linux_arm64.tar.gz";
-      sha256 = "b8d47c08228ea66bb42df02e7559435e3858b75e30e2921f9ef83bf5b43b0c70";
+      sha256 = "1e21904ce69f2acfe1515cb86182ee12b6e14e934418001b6667a238921249e5";
     };
   };
 }

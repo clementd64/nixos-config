4c13d59c5b3f7773c3a469f75d330ec3d9b8d2f3
Clément Dubreuil <clement@dubreuil.dev>
2025-02-10 22:36:25 +0100

    remove miniflux

--- hosts/seebu/system.nix
+++ hosts/seebu/system.nix
@@ -3,7 +3,6 @@
   clement.profile.server.enable = true;
   imports = [
     ./hardware.nix
-    ./miniflux.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -33,35 +32,6 @@
       local  all       postgres           peer
       local  miniflux  miniflux           peer
     '';
-  };
-
-  services.cloudflared = {
-    enable = true;
-    tunnels.aegis = {
-      credentialsFile = "/run/secrets/cloudflared";
-      ingress = {
-        "miniflux.segfault.ovh" = "unix:${config.systemd.sockets.miniflux.socketConfig.ListenStream}";
-      };
-      default = "http_status:404";
-    };
-  };
-
-  clement.secrets.cloudflared = {
-    text = ''
-      -----BEGIN AGE ENCRYPTED FILE-----
-      YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBOM1Y3bkg4bjdPenpIaTRJ
-      amREL1p1MGpXZzk1OE5LaW1zdHlZcGJ2NFVvClNIZ1BSY3h0MUw5dnJqU1ZsL3Bn
-      VUpJbDhDT0pVZDh6QmdQMnlNS0xWRjQKLS0tIHZsTm1KQlVFR3ZDVThGTnZTQTBC
-      SXpqZExjRkhYTkRTODNkdUtLTVRJODQK3GP7GpI6zMUfpGGuCELdpT0o85cmEMdH
-      F2CdOWTXmE0EkegkapchY+Gu5OgAmF397Nb0WvrXkfSfDqwMJLh0MWr6t4f2O5CD
-      Ha/1wxfiUIVqAOtVGlOP+LUWbyMhnae2XBo/w0o3VzbwprIPOl6tkVCwO7hte8ER
-      1G31l/37yPiP3QXRCSUl+WIDK6PKCyA9AynakjITE0z8xYkzpyUdW9FU47qLIifH
-      wShARHMN3ZI+INfAtaHBfqowUsjnkNFbxA==
-      -----END AGE ENCRYPTED FILE-----
-    '';
-    user = "cloudflared";
-    group = "cloudflared";
-    identity = "/nix/agekey";
   };
 
   system.stateVersion = "24.05";

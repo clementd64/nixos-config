c964ff30592eeb50007643f58d7992c9ae4b3415
Clément Dubreuil <clement@dubreuil.dev>
2025-07-29 13:55:15 +0200

    remove seebu

--- flake.nix
+++ flake.nix
@@ -92,10 +92,6 @@
         system = "x86_64-linux";
       };
 
-      seebu = mkStable {
-        system = "aarch64-linux";
-      };
-
       syra = mkUnstable {
         system = "x86_64-linux";
       };

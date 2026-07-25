03b25d0ec36b8b3c14b595a45ab5d2af49c8e192
Clément Dubreuil <clement@dubreuil.dev>
2026-07-25 09:09:55 +0200

    move postgresql to erythia

--- hosts/siren/system.nix
+++ hosts/siren/system.nix
@@ -28,48 +28,5 @@
     size = 2*1024;
   } ];
 
-  clement.secrets = {
-    postgresql-tls-ca = {
-      file = ./secrets.json;
-      extract = ''["postgresql"]["ca"]'';
-      group = "postgres";
-      before = [ "postgresql.service" ];
-    };
-    postgresql-tls-cert = {
-      file = ./secrets.json;
-      extract = ''["postgresql"]["cert"]'';
-      group = "postgres";
-      before = [ "postgresql.service" ];
-    };
-    postgresql-tls-key = {
-      file = ./secrets.json;
-      extract = ''["postgresql"]["key"]'';
-      group = "postgres";
-      before = [ "postgresql.service" ];
-    };
-  };
-
-  services.postgresql = {
-    enable = true;
-    enableJIT = true;
-    enableTCPIP = true;
-    package = pkgs.postgresql_18;
-    authentication = lib.mkOverride 10 ''
-      #type   database  DBuser    address             auth-method
-      local   all       postgres                      peer
-      hostssl miniflux  miniflux  2a0c:b641:2b0::/44  cert
-      hostssl pocketid  pocketid  2a0c:b641:2b0::/44  cert
-    '';
-    settings = {
-      ssl = true;
-      ssl_ca_file = config.clement.secrets."postgresql-tls-ca".path;
-      ssl_cert_file = config.clement.secrets."postgresql-tls-cert".path;
-      ssl_key_file = config.clement.secrets."postgresql-tls-key".path;
-      ssl_min_protocol_version = "TLSv1.3";
-    };
-  };
-
-  clement.firewall.src."tcp:5432" = ["2a0c:b641:2b0::/44"];
-
   system.stateVersion = "25.11";
 }

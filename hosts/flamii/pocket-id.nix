0bc45ec68427e1fcd1b31a4f896526677eec048a
Clément Dubreuil <clement@dubreuil.dev>
2026-07-25 09:24:05 +0200

    pocket-id use dynamic user

--- hosts/flamii/pocket-id.nix
+++ hosts/flamii/pocket-id.nix
@@ -1,19 +1,5 @@
-{ config, pkgs, lib, ... }:
-let
-  settings = {
-    DB_CONNECTION_STRING = "postgresql://pocketid@db.as212625.net/pocketid?sslmode=verify-full&sslcert=${config.clement.secrets."pocket-id-postgresql-tls-cert".path}&sslkey=${config.clement.secrets."pocket-id-postgresql-tls-key".path}&sslrootcert=${config.clement.secrets."pocket-id-postgresql-tls-ca".path}";
-    ENCRYPTION_KEY_FILE = config.clement.secrets."pocket-id-encryption-key".path;
-    HOST = "2a0c:b641:2b2::11";
-    PORT = 443;
-    TLS_CERT = config.clement.acme.certificates."id.dubreuil.dev".cert;
-    TLS_KEY = config.clement.acme.certificates."id.dubreuil.dev".key;
-    APP_URL = "https://id.dubreuil.dev";
-    FILE_BACKEND = "database";
-    UI_CONFIG_DISABLED = true;
-    GEOLITE_DB_URL = "https://github.com/P3TERX/GeoLite.mmdb/releases/latest/download/GeoLite2-City.mmdb";
-    GEOLITE_DB_PATH = "%C/pocket-id/GeoLite2-City.mmdb";
-  };
-in {
+{ config, pkgs, ... }:
+{
   clement.local.addresses = [ "2a0c:b641:2b2::11/128" ];
   clement.firewall.dst."tcp:443" = ["2a0c:b641:2b2::11"];
   clement.firewall.dst."tcp:80" = ["2a0c:b641:2b2::11"];
@@ -22,32 +8,27 @@ in {
     pocket-id-postgresql-tls-ca = {
       file = ./secrets.json;
       extract = ''["pocket-id"]["postgresql"]["ca"]'';
-      group = "pocket-id";
       before = [ "pocket-id.service" ];
     };
     pocket-id-postgresql-tls-cert = {
       file = ./secrets.json;
       extract = ''["pocket-id"]["postgresql"]["cert"]'';
-      group = "pocket-id";
       before = [ "pocket-id.service" ];
     };
     pocket-id-postgresql-tls-key = {
       file = ./secrets.json;
       extract = ''["pocket-id"]["postgresql"]["key"]'';
-      group = "pocket-id";
       before = [ "pocket-id.service" ];
     };
     pocket-id-encryption-key = {
       file = ./secrets.json;
       extract = ''["pocket-id"]["encryption-key"]'';
-      group = "pocket-id";
       before = [ "pocket-id.service" ];
     };
   };
 
   clement.acme.certificates."id.dubreuil.dev" = {
     reload = "pocket-id.service";
-    group = "pocket-id";
   };
 
   systemd.services.pocket-id = {
@@ -61,21 +42,33 @@ in {
     serviceConfig = {
       Type = "notify";
       ExecStart = "${pkgs.pocket-id}/bin/pocket-id";
-      User = "pocket-id";
-      Group = "pocket-id";
+      DynamicUser = true;
       Restart = "always";
       RestartSec = 5;
       CacheDirectory = "pocket-id";
+      LoadCredential = [
+        "postgresql-tls-ca.pem:${config.clement.secrets."pocket-id-postgresql-tls-ca".path}"
+        "postgresql-tls-cert.pem:${config.clement.secrets."pocket-id-postgresql-tls-cert".path}"
+        "postgresql-tls-key.pem:${config.clement.secrets."pocket-id-postgresql-tls-key".path}"
+        "encryption-key:${config.clement.secrets."pocket-id-encryption-key".path}"
+        "tls-cert.pem:${config.clement.acme.certificates."id.dubreuil.dev".cert}"
+        "tls-key.pem:${config.clement.acme.certificates."id.dubreuil.dev".key}"
+      ];
       AmbientCapabilities = "CAP_NET_BIND_SERVICE";
       CapabilityBoundingSet = "CAP_NET_BIND_SERVICE";
     };
-    environment = lib.mapAttrs (_: toString) settings;
+    environment = {
+      DB_CONNECTION_STRING = "postgresql://pocketid@db.as212625.net/pocketid?sslmode=verify-full&sslcert=%d/postgresql-tls-cert.pem&sslkey=%d/postgresql-tls-key.pem&sslrootcert=%d/postgresql-tls-ca.pem";
+      ENCRYPTION_KEY_FILE = "%d/encryption-key";
+      HOST = "2a0c:b641:2b2::11";
+      PORT = "443";
+      TLS_CERT = "%d/tls-cert.pem";
+      TLS_KEY = "%d/tls-key.pem";
+      APP_URL = "https://id.dubreuil.dev";
+      FILE_BACKEND = "database";
+      UI_CONFIG_DISABLED = "true";
+      GEOLITE_DB_URL = "https://github.com/P3TERX/GeoLite.mmdb/releases/latest/download/GeoLite2-City.mmdb";
+      GEOLITE_DB_PATH = "%C/pocket-id/GeoLite2-City.mmdb";
+    };
   };
-
-  users.users.pocket-id = {
-    home = "/var/empty";
-    isSystemUser = true;
-    group = "pocket-id";
-  };
-  users.groups.pocket-id = {};
 }

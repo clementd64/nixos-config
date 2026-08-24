17909505cec5b3ab2addc7844a42ff4dd165f780
Clément Dubreuil <clement@dubreuil.dev>
2026-08-24 19:22:21 +0200

    remove nat64 old host

--- modules/profiles/as212625/default.nix
+++ modules/profiles/as212625/default.nix
@@ -33,11 +33,7 @@ in {
       nat64 = {
         enable = true;
         prefix = "2a0c:b641:2b0::64:0:0/96";
-        allowed = [
-          "2001:bc8:1640:5d6::/64"
-          "2001:bc8:1d90:1a1::/64"
-          "2001:bc8:710:1198::/64"
-        ];
+        allowed = [];
         dns64 = {
           enable = true;
           address = "2a0c:b641:2b0::64:0:53";
